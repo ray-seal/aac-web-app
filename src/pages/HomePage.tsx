13c6bc66ae299b3670db2113931102c6473cfccb
Matt Sheldon <44905900+mattsheldon212@users.noreply.github.com>
2025-08-03 20:49:33 +0100

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,17 +1,13 @@
 // src/pages/HomePage.tsx
 import { Layout } from '../components/Layout'
-import { AACGrid } from '../components/AACGrid'
-import { corePhrases } from '../data/phrases'
+import { ImageSelection } from '../components/ImageSelection'
+import { CommunicationPanel } from '../components/CommunicationPanel'
 
 export default function HomePage() {
-  const handleSelect = (label: string) => {
-    // Add to sentence bar or speak directly
-    console.log('Selected:', label)
-  }
-
   return (
     <Layout>
-      <AACGrid items={corePhrases} onSelect={handleSelect} />
+      <ImageSelection />
+      <CommunicationPanel />
     </Layout>
   )
 }
