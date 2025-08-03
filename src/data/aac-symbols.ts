fa14119227bf617653dc654d42077907ddb41ab3
ray-seal <rayseal1986@gmail.com>
2025-08-04 00:06:14 +0100

    Update aac-symbols.ts

--- src/data/aac-symbols.ts
+++ src/data/aac-symbols.ts
@@ -87,7 +87,7 @@ export const aacSymbols: AacSymbol[] = [
   
   {
     id: 'meds',
-    text: 'meds',
+    text: 'Take meds',
     imagePath: '/aac-images/meds.jpg',
     category: 'actions'
   },
@@ -103,8 +103,111 @@ export const aacSymbols: AacSymbol[] = [
     id: 'yoghurt',
     text: 'Yoghurt',
     imagePath: '/aac-images/yoghurt.jpeg',
+    category: 'food'
+  },
+   {
+    id: 'toilet',
+    text: 'Toilet',
+    imagePath: '/aac-images/toilet.jpg',
     category: 'actions'
   },
+   {
+    id: 'food',
+    text: 'Food',
+    imagePath: '/aac-images/food.jpg',
+    category: 'actions'
+  },
+   {
+    id: 'underwear',
+    text: 'Underwear',
+    imagePath: '/aac-images/underwear.jpg',
+    category: 'actions'
+  },
+   {
+    id: 'tablet',
+    text: 'Tablet',
+    imagePath: '/aac-images/tablet.jpg',
+    category: 'things'
+  },
+ {
+    id: 'school',
+    text: 'Go to school',
+    imagePath: '/aac-images/school.jpg',
+    category: 'places'
+  },
+   {
+    id: 'home',
+    text: 'Go home',
+    imagePath: '/aac-images/home.png',
+    category: 'places'
+  },
+ {
+    id: 'car',
+    text: 'Go in the car',
+    imagePath: '/aac-images/car.jpg',
+    category: 'activities'
+  },
+ {
+    id: 'crisps',
+    text: 'Crisps',
+    imagePath: '/aac-images/crisps.jpg',
+    category: 'food'
+  },
+ {
+    id: 'egg',
+    text: 'Egg',
+    imagePath: '/aac-images/egg.jpg',
+    category: 'food'
+  },
+ {
+    id: 'apple',
+    text: 'Apple',
+    imagePath: '/aac-images/apple.jpg',
+    category: 'food'
+  },
+ {
+    id: 'shoes-on',
+    text: 'Put shoes on',
+    imagePath: '/aac-images/shoes-on.jpg',
+    category: 'actions'
+  },
+ {
+    id: 'chicken',
+    text: 'Chicken',
+    imagePath: '/aac-images/chicken.jpg',
+    category: 'food'
+  },
+ {
+    id: 'macncheese',
+    text: 'Mac N Cheese',
+    imagePath: '/aac-images/macncheese.jpg',
+    category: 'food'
+  },
+ {
+    id: 'bath',
+    text: 'Bath',
+    imagePath: '/aac-images/bath.jpg',
+    category: 'actions'
+  },
+ {
+    id: 'pasta',
+    text: 'Pasta',
+    imagePath: '/aac-images/pasta.jpg',
+    category: 'food'
+  },
+ {
+    id: 'brush hair',
+    text: 'Brush hair',
+    imagePath: '/aac-images/brush-hair.jpg',
+    category: 'actions'
+  },
+ {
+    id: 'brush teeth',
+    text: 'Brush teeth',
+    imagePath: '/aac-images/brush-teeth.jpg',
+    category: 'actions'
+  },
+
 ];
 
 export const categories = ['all', 'greetings', 'needs', 'actions', 'descriptors', 'responses'];
