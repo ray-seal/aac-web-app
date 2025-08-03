8ffed526a1019181a3bb3b4bcf6c2ecf17a078eb
ray-seal <rayseal1986@gmail.com>
2025-08-03 19:13:06 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,8 @@
 import type { NextConfig } from "next";
-import withPWA from "next-pwa";
 
 const nextConfig: NextConfig = {
-  // Your other Next.js config options here
   reactStrictMode: true,
-  // You can add more config options as needed
+  // other config options here if needed
 };
 
-export default withPWA({
-  ...nextConfig,
-  pwa: {
-    dest: "public",
-    disable: process.env.NODE_ENV === "development",
-    register: true,
-    skipWaiting: true,
-  },
-});
+export default nextConfig;
