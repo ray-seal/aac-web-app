a95cb01a6ee0c0781f0ab74df28464519e622cf8
Matt Sheldon <44905900+mattsheldon212@users.noreply.github.com>
2025-08-03 20:20:26 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,23 @@
+// src/App.tsx
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import ImageGallery from './components/ImageGallery'
+import VirtualBoard from './components/VirtualBoard'
+import TextToSpeech from './components/TextToSpeech'
+import CategoryFilter from './components/CategoryFilter'
+import { aacSymbols, categories, AacSymbol } from './data/aac-symbols'
+import { HomePage } from './pages/HomePage'
 
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+
+        {/* Catch-all must go last */}
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
