be3c3429fabb34c0a3d7576b0b80acdebe6492e7
Matt Sheldon <44905900+mattsheldon212@users.noreply.github.com>
2025-08-03 20:52:54 +0100

    Update speak.ts

--- src/utils/speak.ts
+++ src/utils/speak.ts
@@ -1,6 +1,16 @@
 // src/utils/speak.ts
 export const speak = (text: string) => {
+  if (!('speechSynthesis' in window)) {
+    alert('Sorry, your browser does not support speech synthesis.')
+    return
+  }
+
   const utterance = new SpeechSynthesisUtterance(text)
-  utterance.lang = 'en-US'
-  speechSynthesis.speak(utterance)
+  utterance.lang = 'en-GB' // You can customize this
+  utterance.rate = 1 // Speed (0.1 to 10)
+  utterance.pitch = 1 // Pitch (0 to 2)
+  utterance.volume = 1 // Volume (0 to 1)
+
+  window.speechSynthesis.cancel() // Optional: cancel any queued speech
+  window.speechSynthesis.speak(utterance)
 }
