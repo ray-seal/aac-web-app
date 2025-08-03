59a9c42aaea5d65aab4769526d7f742a4fe7b346
Matt Sheldon <44905900+mattsheldon212@users.noreply.github.com>
2025-08-03 21:29:55 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 // src/App.tsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import ImageGallery from './components/ImageGallery'
-import VirtualBoard from './components/VirtualBoard'
-import TextToSpeech from './components/TextToSpeech'
-import CategoryFilter from './components/CategoryFilter'
-import { aacSymbols, categories, AacSymbol } from './data/aac-symbols'
 import { HomePage } from './pages/HomePage'
 
 function App() {
