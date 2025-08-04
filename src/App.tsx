9076118786dad520c00f77be391d0b53aad191b0
ray-seal <rayseal1986@gmail.com>
2025-08-05 00:55:07 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-// src/App.tsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import { NotFoundPage } from './pages/NotFoundPage'
+import { Parent } from './pages/Parent'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-
+        <Route path="/parent" element={<Parent />} />
         {/* Catch-all must go last */}
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
