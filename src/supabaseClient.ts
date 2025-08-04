5019f7fae9c2be86bd2b07c27b0b3fb78a3b4aeb
ray-seal <rayseal1986@gmail.com>
2025-08-05 00:46:36 +0100

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = https://ufarjxahabeesvtrtsej.supabase.co'; 
+const supabaseUrl = 'https://ufarjxahabeesvtrtsej.supabase.co'; 
 const supabaseKey = 'sb_publishable_AGcJhcbBQ4Wp9J4JVzMJMA_TIrOka5-'; 
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
