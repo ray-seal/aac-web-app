fa65b8564d964e2b4814f8c4f0657df2d794c49d
ray-seal <rayseal1986@gmail.com>
2025-08-05 01:22:02 +0100

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,3 @@
-import React, { useEffect, useState } from 'react'
 import { AACGrid } from '../components/AACGrid'
 import { aacSymbols } from '../data/aac-symbols'
 import { supabase } from '../supabaseClient'
