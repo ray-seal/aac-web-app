4135a30f011a52972a8b2d35dc9dbcf0fb3899ad
ray-seal <rayseal1986@gmail.com>
2025-08-10 03:50:45 +0100

    Update workbox-config.js

--- workbox-config.js
+++ workbox-config.js
@@ -4,15 +4,18 @@ module.exports = {
     '**/*.{html,js,css,png,jpg,json,svg}'
   ],
   swDest: 'dist/sw.js',
-  runtimeCaching: [{
-    urlPattern: /\.(?:png|jpg|jpeg|svg|gif)$/,
-    handler: 'CacheFirst',
-    options: {
-      cacheName: 'images',
-      expiration: {
-        maxEntries: 50,
-        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+  mode: 'no-minify', // Disable Terser/minification to avoid Termux/Vite/Terser write errors
+  runtimeCaching: [
+    {
+      urlPattern: /\.(?:png|jpg|jpeg|svg|gif)$/,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'images',
+        expiration: {
+          maxEntries: 50,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+        },
       },
-    },
-  }],
+    }
+  ],
 };
