53f891fc17e29043ccdec0639ff7d6793fe1c0f1
ray-seal <rayseal1986@gmail.com>
2025-08-10 01:38:26 +0100

    Update howTo.tsx

--- src/pages/howTo.tsx
+++ src/pages/howTo.tsx
@@ -9,11 +9,11 @@ const HowToPage: React.FC = () => {
         ← Back to app
       </button>
       {/* Edit from here down */}
-      <h1 text-align: center;>
+      <h1 text-align: center>
         <b>Speakly How To</b>
       </h1>
       <div>
-        <p text-align: center;>
+        <p text-align: center>
           Welcome to Speakly. The voice given to those who cannot speak for themselves.
         </p>
         <ul>
