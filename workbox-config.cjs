379c4105820c1915e241998a28767570388892b8
ray-seal <rayseal1986@gmail.com>
2025-08-10 04:01:37 +0100

    Update and rename workbox-config.js to workbox-config.cjs

---
+++ workbox-config.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  globDirectory: 'dist/',
+  globPatterns: [
+    '**/*.{html,js,css,png,jpg,json,svg}'
+  ],
+  swDest: 'dist/sw.js',
+  mode: 'no-minify',
+  runtimeCaching: [{
+    urlPattern: /\.(?:png|jpg|jpeg|svg|gif)$/,
+    handler: 'CacheFirst',
+    options: {
+      cacheName: 'images',
+      expiration: {
+        maxEntries: 50,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      },
+    },
+  }],
+};
